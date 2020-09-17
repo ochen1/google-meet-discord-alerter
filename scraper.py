d0528053d4674bbe9358b881dc791c2fe9e8dc91
Oliver Chen <o.chen1@share.epsb.ca>
2020-09-17 10:06:16 -0600

    Fix bugs and switch to pyvirtualdisplay instead of headless chrome (causes error with Google login)

--- scraper.py
+++ scraper.py
@@ -5,11 +5,11 @@ from assertGoogleLogin import assertLogin
 from selenium import webdriver
 from traceback import print_exc
 from os.path import dirname, abspath
+from pyvirtualdisplay import Display
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--headless")
 chrome_options.add_argument(
     "--user-data-dir=" + os.path.abspath(os.getenv('VPROF_PATH')))
 chrome_options.binary_location = os.getenv('GOOGLE_CHROME_PATH')
@@ -17,6 +17,8 @@ chrome_options.binary_location = os.getenv('GOOGLE_CHROME_PATH')
 
 def getNumPeople(mid):
     try:
+        display = Display(visible=False, size=(800, 600))
+        display.start()
         driver = webdriver.Chrome(
             executable_path=os.getenv('CHROMEDRIVER_PATH'),
             options=chrome_options
@@ -24,7 +26,7 @@ def getNumPeople(mid):
         try:
             assertLogin(driver)
         except AssertionError:
-            return 0
+            return '[error]'
         driver.get("https://meet.google.com/lookup/%s" % mid)
         sleep(2)
         v = driver.execute_script("""
@@ -38,9 +40,17 @@ def getNumPeople(mid):
         return 1;
     }
     """)
-        driver.close()
     except Exception:
         with open(dirname(abspath(__file__)).rstrip('/') + '/logs.log', 'a') as logf:
             print_exc(file=logf)
         v = '[error]'
+    finally:
+        try:
+            driver.close()
+        except:
+            pass
+        try:
+            display.stop()
+        except:
+            pass
     return v
